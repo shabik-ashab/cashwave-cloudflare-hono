ae2ea192b10429f1652370d793026eed85a42a3d
nub69 <shabik.ashab@gmail.com>
2024-04-01 16:28:27 +0600

    test prisma accelarate for serverless

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
-import { Hono } from 'hono'
+import { Hono, Next } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { env } from 'hono/adapter'
 
 const app = new Hono()
 
-app.get('/', (c) => { 
-  return c.text('Hello Hono!')
+app.post('/', async (c) => {
+  // add zod validation here
+  const body: {
+    name: string;
+    email: string;
+    password: string
+  } = await c.req.json()
+  const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c)
+
+  const prisma = new PrismaClient({
+      datasourceUrl: DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  console.log(body)
+
+  const user = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      password: body.password
+    }
+  })
+  
+  return c.json(user)
 })
 
 export default app
