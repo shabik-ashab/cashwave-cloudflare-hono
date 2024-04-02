e81715d5ed56bef67f3ff767bd0ba83d6362ece5
nub69 <shabik.ashab@gmail.com>
2024-04-02 20:54:09 +0600

    get balance added

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,6 +1,11 @@
 import { Hono } from 'hono'
+import { authMiddleware } from '../middleware'
+import getBalance from './helper/getBalance'
+
 const app = new Hono()
 
 app.get('/', (c) => c.text('account'))
+
+app.get("/balance", authMiddleware, getBalance)
 
 export default app
---
+++ src/routes/helper/getBalance.ts
@@ -0,0 +1,39 @@
+import getPrismaClient from '../../prisma';
+
+const getBalance = async (c) => {
+    try {
+        // Assuming req.userId is extracted from the request, you can use c.req.userId instead
+        const userId = c.req.userId;
+
+        const prisma = getPrismaClient(c);
+
+        const account = await prisma.account.findFirst({
+            where: {
+                userId: userId
+            }
+        });
+
+        if (!account) {
+            return c.json({
+                statusCode: 404,
+                body: {
+                    message: "Account not found"
+                }
+            });
+        }
+
+        return c.json({
+            balance: account.balance
+        });
+    } catch (error) {
+        console.error("Error occurred while fetching account:", error);
+        return c.json({
+            statusCode: 500,
+            body: {
+                message: "Internal server error"
+            }
+        });
+    }
+}
+
+export default getBalance
